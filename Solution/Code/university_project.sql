78ffa754b356f5a70a5acf9098c3ac28963cae16
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-04-27 10:19:30 +0300

    Update university_project.sql

--- Solution/Code/university_project.sql
+++ Solution/Code/university_project.sql
@@ -201,24 +201,24 @@ CONSTRAINT FOREIGN KEY (album_id) REFERENCES albums(id)
 
 
 INSERT INTO composer (name, egn, nationality, phone, dateOfBirth) 
-VALUES ('Johann Sebastian Bach', '0912934563', 'German', NULL, '1685-03-21'),
+VALUES ('Hanz Zimmer', '0912934563', 'German', '455-9009', '1957-09-12'),
 	   ('Ed Sheeran', '0342673901', 'British', '550-0673', '1991-02-17'),
 	   ('Toots Thielemans', '0067890123', 'German', NULL, '1922-04-29'),
        ('Manu Chao', '3450729113', 'French', '645-2480', '1961-06-21'),
 	   ('Freddie Mercury', '0608901230', 'British', NULL, '1946-09-05'),
-	   ('Igor Stravinsky', '6789012345', 'Russian', NULL, '1882-06-17'),
+	   ('Alexandre Desplat', '6789012345', 'French', NULL, '1961-08-23'),
 	   ('Drake', '7890123050', 'American', '535-8903', '1986-10-24'),
-	   ('Maurice Ravel', '8901934560', 'French', NULL, '1875-03-07'),
-	   ('Kanye West', '9012341638', 'American', '989-0909', '1906-09-25'),
-	   ('John Williams', '9897866540', 'American', '555-2468', '1932-02-08'),
-	   ('Philip Glass', '2146570901', 'American', '089-3221', '1937-01-31'),
+	   ('Joe Dassin', '8901934560', 'French', NULL, '1938-11-05'),
+	   ('Kanye West', '9012341638', 'American', '989-0909', '1977-06-08'),
+	   ('Charles Aznavour', '9897866540', 'French', NULL, '1924-05-22'),
+	   ('Philip Glass', '2146570901', 'American', NULL, '1937-01-31'),
 	   ('Elvis Presley', '0459789032', 'American', NULL, '1935-01-08');
 
 
 INSERT INTO userRole
 VALUES (NULL, 'Admin'),
        (NULL, 'Customer');
-
+       
 
 INSERT INTO user (username, password, egn, address, phone, userRole_id)
 VALUES ('John Brown', 'H@rdT0Gu3ss!', '0123426739', '123 Main St, Anytown', '353-1333', 2),
@@ -231,21 +231,21 @@ VALUES ('John Brown', 'H@rdT0Gu3ss!', '0123426739', '123 Main St, Anytown', '353
 	   ('Robert James', 'M9pBcT!z', '8901257547', '87A Birch Ln, Otherville', NULL, 2),
 	   ('Ekaterina Valova', '1#jGHTk$', '9012245778', '133 Elm St, Somewhere', '989-3460', 2),
 	   ('Beatris Grivardis', 'F7nGkP!z', '0103459701', '454 Maple Ave, Anytown', NULL, 1);
-
+	
 
 INSERT INTO orders (order_date, price, payment_status, delivery_status, user_id)
-VALUES ('2023-03-25', 10.50, 'paid', 'delivered', 1),
+VALUES ('2023-03-25', 20.50, 'paid', 'delivered', 1),
        ('2023-03-26', 25.80, 'paid', 'delivered', 2),
-       ('2023-03-26', 42.00, 'paid', 'delivered', 2),
+       ('2023-03-26', 92.00, 'paid', 'delivered', 2),
        ('2023-03-27', 15.00, 'paid', 'delivered', 3),
-       ('2023-04-03', 56.30, 'paid', 'in transit', 4),
+       ('2023-04-03', 205.30, 'paid', 'in transit', 4),
        ('2023-04-04', 12.75, 'paid', 'in transit', 5),
-	   ('2023-04-07', 37.20, 'pending', 'in transit', 5),
+	   ('2023-04-07', 37.20, 'pending', 'pending', 5),
        ('2023-04-07', 19.90, 'paid', 'in transit', 5),
-       ('2023-04-07', 82.60, 'paid', 'in transit', 6),
+       ('2023-04-07', 150.60, 'paid', 'in transit', 6),
        ('2023-04-10', 45.30, 'paid', 'delivered', 7),
        ('2023-04-10', 45.30, 'paid', 'delivered', 8),
-       ('2023-04-12', 45.30, 'pending', 'delivered', 9),
+       ('2023-04-12', 245.30, 'pending', 'not shipped', 9),
        ('2023-04-15', 45.30, 'pending', 'not shipped', 10);
        
        
@@ -284,7 +284,7 @@ VALUES (NULL, 'Drake', '9124656742', '111 Pine St San Francisco, CA 94111', '098
        (NULL, 'Michael Jackson', '8909234560', '2222 Oak Ave Anytown, USA', '099-4369', '1958-08-29'),
        (NULL, 'Miles Davis', '3202428290', '5555 Pine Ln Anytown, USA', '198-5141', '1926-05-26'),
        (NULL, 'Macklemore', '4545009877', '6767 Cedar Rd Anytown, USA', NULL, '1983-06-19'),
-       (NULL, 'Kanye West', '9012341638', '2727 Oak St Springfield, USA', '108-4362', '1906-09-25'),
+       (NULL, 'Kanye West', '9012341638', '2727 Oak St Springfield, USA', '108-4362', '1977-06-08'),
        (NULL, 'John Legend', '3402921045', '2424 Birch Ct Anytown, USA', '028-1285', '1978-12-28'),
        (NULL, 'Billie Holiday', '3003210981', '7878 Oak Ln Springfield, USA', NULL,  '1915-07-17'),
 	   (NULL, 'Joseph Satriani', '3059210292', '7880 Oak Ln Springfield, USA', '201-4369', '1956-07-15');       
@@ -293,21 +293,20 @@ VALUES (NULL, 'Drake', '9124656742', '111 Pine St San Francisco, CA 94111', '098
 INSERT INTO albums
 VALUES (NULL, 'High Off Life', 21, 80.20, '2020-05-15', 'Republic Records', 1),
        (NULL, 'Are You Experienced', 8, 42.39, '1969-10-22', 'Atlantic Records', 3),
-       (NULL, 'Divide', 11, 50.13, '2017-03-03', 'Asylum', 4),
+       (NULL, 'Divide', 11, 50.03, '2017-03-03', 'Asylum', 4),
        (NULL, 'GEMINI', 16, 60.13, '2017-09-22', 'Bendo LLC', 10),
 	   (NULL, 'The Orange Room', 15, 55.29, '2005-03-01', 'Sheeran Lock', 4),
-	   (NULL, '7', 15, 43.29, '2021-09-17', 'Columbia Records', 7),
+	   (NULL, '7', 15, 43.37, '2021-09-17', 'Columbia Records', 7),
        (NULL, 'Thriller', 9, 42.19, '1982-11-30', 'Epic Records', 8),
        (NULL, 'Kind of Blue', 5, 45.52, '1959-08-17', 'Columbia Records', 9),
        (NULL, 'Scorpion', 25, 89.04, '2018-06-29', 'Cash Money Records', 1),
        (NULL, 'Future', 19, 66.14, '2017-02-17', 'Interscope Records', 2),
 	   (NULL, 'Graduation', 13, 51.23, '2008-08-19', 'KonLive', 11),
-	   (NULL, 'Wake Up', 13, 51.23, '2008-08-19', 'Domino Recording Company', 12),
-	   (NULL, 'All or Nothing at All', 15, 32.14, '1938-08-19', 'Sony Music', 13),
-	   (NULL, 'The Elephants of Mars', 10, 59.14, '1938-08-19', 'Universal Music Group', 14),
-	   (NULL, 'Hello Dolly', 11, 40.14, '1938-08-19', 'Warner Music', 6),
+	   (NULL, 'Wake Up', 13, 51.13, '2008-08-19', 'Domino Recording Company', 12),
+	   (NULL, 'All or Nothing at All', 15, 32.48, '1938-08-19', 'Sony Music', 13),
+	   (NULL, 'The Elephants of Mars', 10, 59.04, '1938-08-19', 'Universal Music Group', 14),
+	   (NULL, 'Hello Dolly', 11, 40.24, '1938-08-19', 'Warner Music', 6),
 	   (NULL, 'Journeyman', 12, 42.32, '1938-08-19', 'XL Recordings', 5);
-       
        
        
 INSERT INTO song
@@ -322,7 +321,7 @@ VALUES (NULL ,'Shape Of You', 'https://www.youtube.com/watch?v=JGwWNGJdvx8', 'Po
        (NULL, 'Addicted', 'https://www.youtube.com/watch?v=6YS03WG2t1Q&list=PLT3w2jU3eU6nF1HzjTtKYzvMhJIvfQ8me', 'Latin Pop', 'Piano', 320, 10500321, 5),
 	   (NULL, 'Sweet Child O Mine', 'https://www.youtube.com/watch?v=1w7OgIMMRc4', 'Hard rock', 'Electronic', 454, 1234000989, 16),
        (NULL, 'So What', 'https://www.youtube.com/watch?v=ylXk1LBvIqU&list=PLrhkpF1bKMG_D2sxTpxG63WGmIxYGB-Jt', 'Pop rock', 'Electronic', 220, 415609909, 8),
-       (NULL, 'Someday', 'https://www.youtube.com/watch?v=ayxLRvQOni0&list=PL0a-3E47mvaZsK839s_pQSICTPnfKGGT9&index=5', 'Pop', 'Electronic', 640, 350000, 15),
+       (NULL, 'Someday', 'https://www.youtube.com/watch?v=ayxLRvQOni0&list=PL0a-3E47mvaZsK839s_pQSICTPnfKGGT9&index=5', 'Jazz', 'Swing', 640, 350000, 15),
        (NULL, 'Ten Million', 'https://www.youtube.com/watch?v=24cCemVI1CY&list=PLCJjdqofvBsUJzotAzzXSxleSnjZpUCXp&index=10', 'Hip-hop', 'Electronic', 179, 1100000, 5),
        (NULL, 'In My Feelings', 'https://www.youtube.com/watch?v=DRS_PpOrUZ4', 'Hip-Hop', 'Electronic', 217, 160000000, 1),
        (NULL, 'Stairway To Heaven', 'https://www.youtube.com/watch?v=QkF3oxziUI4', 'Rock', 'Electronic', 481, 56000000, 2),
@@ -331,7 +330,7 @@ VALUES (NULL ,'Shape Of You', 'https://www.youtube.com/watch?v=JGwWNGJdvx8', 'Po
 	   (NULL, 'Nonstop', 'https://www.youtube.com/watch?v=XNpGNykVZ6U&list=PLnHe1KWzAptFfF5UTOSuZWsCI2EXz_PHF&index=3', 'Hip-hop', 'Electronic', 389, 203200430, 9),
 	   (NULL, 'Zoom', 'https://www.youtube.com/watch?v=q9HCGqByjak', 'Hip-hop', 'Electronic', 325, 20200300, 10),
 	   (NULL, 'But Not For Me', 'https://www.youtube.com/watch?v=TUVdBKsgq2o', 'Jazz', 'Electronic', 232, 125000, 13);
-
+       
 	
 INSERT INTO reviews (id, rating, comment, review_date, user_id, album_id)
 VALUES (NULL, 8.5, 'Great album, loved it!', '2023-02-20', 1, 1),
@@ -344,6 +343,7 @@ VALUES (NULL, 8.5, 'Great album, loved it!', '2023-02-20', 1, 1),
        (NULL, 9.5, 'Absolutely amazing album, highly recommend', '2023-04-05', 8, 9),
        (NULL, 7.8, 'Good album, but not as good as their previous work', '2023-04-10', 9, 10),
 	   (NULL, 6.1, 'Not a fan of this album, couldn''t get into it', '2023-04-12', 10, 11);  
+       
        
        
 INSERT INTO sales (sale_date, sale_price, orders_id) 
@@ -365,16 +365,17 @@ VALUES (4, 1),
        (10, 6),
        (11, 6);
        
+       
 INSERT INTO performer_genre
 VALUES (1, 3),
        (1, 4),
-       (1, 4),
+       (11, 4),
        (2, 4);
        
 INSERT INTO song_genre
 VALUES (5, 1),
        (5, 2),
-       (5, 3),
+       (8, 3),
        (6, 3);
        
 
@@ -382,7 +383,9 @@ INSERT INTO composer_song
 VALUES (3, 9),
        (3, 11),
        (10, 2),
-       (11, 2);
+       (11, 2),
+       (9, 1),
+       (8, 1);
 
 
 INSERT INTO composer_albums 
@@ -390,6 +393,7 @@ VALUES (2, 4),
        (3, 4),
 	   (12, 3),
 	   (12, 5);
+       
        
 INSERT INTO album_genre
 VALUES (1, 3),
@@ -401,7 +405,7 @@ INSERT INTO playlist_album
 VALUES (2, 1),
 	   (2, 13),
        (1, 2),
-       (1, 2);
+       (3, 2);
        
 INSERT INTO playlist_user
 VALUES (2, 3),
@@ -426,3 +430,27 @@ VALUES (4, 2),
 	   (4, 3),
        (7, 5),
 	   (9, 5);
+       
+/*Напишете заявка, с която извеждате всички песни, които са написани от даден композитор, заедно с информация за композитора:*/
+
+/*SELECT song.name AS songName, composer.name AS composerName, composer.egn AS EGN, composer.nationality AS nationality, composer.phone AS MPhone, composer.dateOfBirth AS dateOfBirth
+FROM song 
+JOIN composer_song ON song.id = composer_song.song_id
+JOIN composer ON composer.id = composer_song.composer_id
+WHERE composer.id = 3;*/
+
+/*Напишете заявка, с която извеждате всички песни, които са написани от повече от един композитор, като са подредени в азбучен ред по заглавие:*/
+/*SELECT song.name AS nameOfSong
+FROM song
+LEFT JOIN composer_song ON song.id = composer_song.song_id
+GROUP BY song.id
+HAVING COUNT(composer_song.composer_id) > 1
+ORDER BY song.name ASC;*/
+
+/*Напишете заявка с която извеждате всички продажби, направени на 11 март 2023 година, 
+като покажете и информацията за албумите, които са били закупени:*/
+
+/*SELECT sales.id, sales.sale_price AS price, albums.title, albums.record_label
+FROM sales JOIN sale_album ON sales.id = sale_album.sale_id 
+JOIN albums ON sale_album.album_id = albums.id
+WHERE sales.sale_date = '2023-03-11';*/
