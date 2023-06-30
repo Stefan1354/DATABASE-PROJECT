d4303cb47770c767216af475e2c81493259a4f04
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-07-01 00:39:20 +0300

    Update university_project.sql

--- Solution/Code/university_project.sql
+++ Solution/Code/university_project.sql
@@ -591,7 +591,6 @@ ORDER BY song.numberOfViews DESC
 LIMIT 10;
 
 /*Напишете заявка, която извежда имената, авторите и жанровете на 10-те най-продавани песни*/
-    ORDER BY
     LIMIT 10;*/
 
 SELECT 
